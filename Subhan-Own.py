7843fd2bcac791d4103b0b7e6f5c74924e145bbd
Subhan khan <98511230+SubhanXKhan@users.noreply.github.com>
2022-02-18 01:27:46 +0500

    Update Subhan-Own.py

--- Subhan-Own.py
+++ Subhan-Own.py
@@ -5,8 +5,8 @@
 # All Thing Is possible But You Need  Hard work
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
 os.system('rm -rf .txt')
-/for n in range(400000):
-nmbr = random.randint(1111111, 9999999)
+/for n in range(40000):
+nmbr = random.randint(111111, 999999)
 sys.stdout = open('.txt',
 'a')
 print nmbr
